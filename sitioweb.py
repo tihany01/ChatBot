774446deef03e0ba70cb521dc67b119656b3c90b
tihany01 <trocabadoseverich@gmail.com>
2023-09-14 13:17:20 -0400

    Mejorar sitioweb 01

--- sitioweb.py
+++ sitioweb.py
@@ -2,31 +2,33 @@ import os
 import re
 from langchain.document_loaders import WebBaseLoader
 
-# Leer el archivo que contiene los enlaces
+# Read the file containing the links
 with open("lista_de_sitios.txt", "r") as f:
-    urls = f.readlines()
+    urls = [url.strip() for url in f.readlines()]  # Remove any trailing whitespace or newline
 
-# Crear el directorio si no existe
-os.makedirs('informacion', exist_ok=True)
+# Create the directory if it doesn't exist
+os.makedirs('information', exist_ok=True)
 
-# Iterar a través de cada enlace
+# Iterate through each link
 for i, url in enumerate(urls):
-    url = url.strip()  # Eliminar cualquier espacio en blanco o nueva línea al final
-
-    # Cargar la página web
+    # Load the web page
     loader = WebBaseLoader(url)
     docs = loader.load()
 
-    # Limpiar el texto
-    cleaned_text = re.sub('<.*?>', '', docs[0].page_content)
-    cleaned_text = re.sub('[^A-Za-z0-9áéíóúÁÉÍÓÚñÑ\s]+', '', cleaned_text)
-    cleaned_text = re.sub('\s+', ' ', cleaned_text).strip()
+    # Clean the text
+    cleaned_text = re.sub('<.*?>', '', docs[0].page_content)  # Remove HTML tags
+    cleaned_text = re.sub('[^A-Za-z0-9áéíóúÁÉÍÓÚñÑ\s]+', '', cleaned_text)  # Remove non-alphanumeric characters except spaces and Spanish accents
+    cleaned_text = re.sub('\s+', ' ', cleaned_text).strip()  # Replace multiple spaces with a single space
 
-    # Contar palabras
+    # Count words
     word_count = len(cleaned_text.split())
 
-    # Guardar el contenido en un archivo
-    with open(f'informacion/sitioweb{i+1}.txt', 'w', encoding='utf-8') as f:
-        f.write(cleaned_text)
+    # Check if the file already exists to avoid repetition
+    filename = f'informacion/sitioweb{i+1}.txt'
+    if not os.path.exists(filename):
+        # Save the content in a file
+        with open(filename, 'w', encoding='utf-8') as f:
+            f.write(cleaned_text)
 
-    print(f"El documento {i+1} tiene {word_count} palabras.")
+        print(f"El documento {i+1} tiene {word_count} palabras.")
+
