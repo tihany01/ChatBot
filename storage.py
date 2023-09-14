303e86ba3629f91e6f920d03e11a073d90e1b0ef
tihany01 <trocabadoseverich@gmail.com>
2023-09-14 16:01:58 -0400

    Mejorar storage.py

--- storage.py
+++ storage.py
@@ -7,42 +7,49 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 
 
 load_dotenv(find_dotenv())
-
 os.environ["OPENAI_API_KEY"] = os.environ['OPENAI_API_KEY']
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
+# Define the directory where the vector files will be saved
+vector_directory = 'vectores/'
 
-directorio_vectores = 'vectores/'
+# Create the directory if it does not exist
+if not os.path.exists(vector_directory):
+    os.makedirs(vector_directory)
 
-# Crea el directorio si no existe
-if not os.path.exists(directorio_vectores):
-    os.makedirs(directorio_vectores)
+# Define the directory where the chunked text files are located
+chunks_directory = 'fragmentos/'
 
-# Define el directorio donde están los archivos de texto fragmentados
-directorio_fragmentos = 'fragmentos/'
-
-# Inicializa los embeddings de OpenAI
+# Initialize OpenAI embeddings
 embeddings = OpenAIEmbeddings()
 
-# Recorre cada archivo en el directorio de fragmentos
-for archivo in os.listdir(directorio_fragmentos):
-    if archivo.endswith('.txt'):
-        texto = None
+# Go through each file in the chunks directory
+for file in os.listdir(chunks_directory):
+    if file.endswith('.txt'):
+        # Check if vector files for this chunked text file already exist
+        vector_files_exist = any(fname.startswith(file[:-4]) and fname.endswith('.npy') for fname in os.listdir(vector_directory))
+        
+        # If vector files exist, skip this chunked text file
+        if vector_files_exist:
+            print(f"Vector files for '{file}' already exist, skipping...")
+            continue
+
+        text = None
         try:
-            with open(os.path.join(directorio_fragmentos, archivo), 'r', encoding='utf-8') as f:
-                texto = f.read()
+            with open(os.path.join(chunks_directory, file), 'r', encoding='utf-8') as f:
+                text = f.read()
         except UnicodeDecodeError:
             try:
-                with open(os.path.join(directorio_fragmentos, archivo), 'r', encoding='latin-1') as f:
-                    texto = f.read()
+                with open(os.path.join(chunks_directory, file), 'r', encoding='latin-1') as f:
+                    text = f.read()
             except Exception as e:
-                print(f"Error al decodificar el archivo {archivo} con UTF-8 y latin-1. Omitiendo... Error: {e}")
+                print(f"Error decoding file {file} with UTF-8 and latin-1. Skipping... Error: {e}")
                 continue
 
-        if texto:
-            # Obtiene los embeddings para el fragmento de texto
-            embedding = embeddings.embed_query(texto)
+        if text:
+            # Get embeddings for the chunk of text
+            embedding = embeddings.embed_query(text)
             
-            # Guarda el embedding en un nuevo archivo
-            nombre_embedding = f"{archivo[:-4]}_embedding.npy"
-            np.save(os.path.join(directorio_vectores, nombre_embedding), embedding)
+            # Save the embedding in a new file
+            embedding_name = f"{file[:-4]}_embedding.npy"
+            np.save(os.path.join(vector_directory, embedding_name), embedding)
