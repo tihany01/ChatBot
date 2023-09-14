0af8f8a1ac26c6be9ce52ba7cee8c2d92cb50527
tihany01 <trocabadoseverich@gmail.com>
2023-09-14 18:08:11 -0400

    Mejorar app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request, jsonify
-# Suponiendo que la función main está definida en un módulo llamado LLM
+# Import the main function from the LLM module. 
 from LLM import main  
 from flask_cors import CORS
+from waitress import serve
 
 app = Flask(__name__)
 CORS(app)
@@ -9,12 +10,14 @@ CORS(app)
 @app.route('/api/generar_respuesta', methods=['POST'])
 def generar_respuesta():
     try:
+        # Extract the 'consulta' field from the incoming JSON request.
         consulta = request.json['consulta']
+         # Call the main function (imported from LLM) with consulta as an argument and store the result in respuesta.
         respuesta = main(consulta)
+        # Return the 'respuesta' wrapped in JSON format.
         return jsonify({'respuesta': respuesta})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(port=5000)
-
+    serve(app, host="0.0.0.0", port=5000)
