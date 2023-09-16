09d14473f2df88745deed76319edf11e46aba651
tihany01 <trocabadoseverich@gmail.com>
2023-09-16 08:41:47 -0400

    ultimo comit MODIFICANDO LLM para el prompt no vuelva vacio en todo caso de no encontrar coinsidencia en vectores

--- LLM.py
+++ LLM.py
@@ -5,9 +5,63 @@ from dotenv import load_dotenv, find_dotenv
 import openai
 from sklearn.metrics.pairwise import cosine_similarity
 from langchain.embeddings.openai import OpenAIEmbeddings  
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 load_dotenv(find_dotenv())
 openai.api_key = os.environ["OPENAI_API_KEY"]
+# Function to save data in AnzoGraph
+def guardar_en_anzograph(consulta, respuesta):
+    endpoint_url = "http://localhost:80/sparql"
+
+    username = "admin"
+    password = "Passw0rd1"
+
+    sparql = SPARQLWrapper(endpoint_url)
+    sparql.setCredentials(username, password)
+    sparql.setMethod("POST")  # Set method to POST
+    
+    # Format your SPARQL query to save the data here
+    consulta_sparql = f"""
+        INSERT DATA {{
+            <http://example.com/{consulta}> <http://example.com/respuesta> "{respuesta}" .
+        }}
+    """
+    
+    sparql.setQuery(consulta_sparql)
+    
+    try:
+        sparql.query()
+        print("Datos guardados en AnzoGraph exitosamente.")
+    except Exception as e:
+        print(f"Error al guardar datos en AnzoGraph: {e}")
+
+# Function to retrieve data from AnzoGraph
+def recuperar_de_anzograph():
+    endpoint_url = "http://localhost:80/sparql"  
+    username = "admin"
+    password = "Passw0rd1"
+
+    sparql = SPARQLWrapper(endpoint_url)
+    sparql.setCredentials(username, password)
+    sparql.setMethod("GET")  # Use GET for SELECT queries
+
+    consulta_sparql = "SELECT * WHERE { ?s ?p ?o } LIMIT 20"  
+    
+    sparql.setQuery(consulta_sparql)
+    sparql.setReturnFormat(JSON)
+
+    try:
+        resultados = sparql.query().convert()
+        print("Resultado completo:")
+        print(resultados) # Print the entire result for debugging
+
+        print("Resultados recuperados de AnzoGraph:")
+        for resultado in resultados['results']['bindings']:
+            print(resultado)
+    except Exception as e:
+        print(f"Error al recuperar datos de AnzoGraph: {e}")
+
+
 # Function to generate a response using the GPT-3 model
 def generar_respuesta_gpt3(pregunta):
     try:
@@ -54,7 +108,7 @@ def encontrar_vector_similar(vector_consulta, vectores):
     std_similitud = np.std(similitudes)
     
     # Establecer el umbral de similitud de manera dinámica
-    umbral_similitud = media_similitud + (0.6 * std_similitud)
+    umbral_similitud = media_similitud + (3* std_similitud)
     # Return the most similar vector, its similarity, and the threshold
     indice_max_similitud = np.argmax(similitudes)
     return vectores_nombre[indice_max_similitud], similitudes[indice_max_similitud], umbral_similitud
@@ -77,18 +131,21 @@ def procesar_consulta(vector_consulta, vectores, consulta_usuario):
     # Find the most similar vector
     nombre_archivo_vector, max_similitud, umbral_similitud = encontrar_vector_similar(vector_consulta, vectores)
 
-    if max_similitud > umbral_similitud:
-        fragmento = buscar_fragmento(nombre_archivo_vector)
-        if fragmento:
-            # Generar un prompt más explícito para GPT-3
-            prompt = f"El usuario preguntó: '{consulta_usuario}'. Basándote en la siguiente información que encontré: '{fragmento}', ¿podrías proporcionar una respuesta?"
-            respuesta = generar_respuesta_gpt3(prompt)
-            return respuesta if respuesta else "Lo siento, no pude encontrar la información relacionada."
-        else:
-            return "Lo siento, no pude encontrar la información relacionada."
+    # Check if the maximum similarity is above the threshold
+    if max_similitud < umbral_similitud:
+        return "Lo siento, no pude encontrar información relevante sobre tu consulta."
+    
+    fragmento = buscar_fragmento(nombre_archivo_vector)
+    if fragmento:
+        # Imprimir el nombre del archivo del fragmento
+        print(f"Respondiendo con el fragmento: {nombre_archivo_vector}")
+        # Generar un prompt más explícito para GPT-3
+        prompt = f"El usuario preguntó: '{consulta_usuario}'. Basándote en la siguiente información que encontré: '{fragmento}', ¿podrías proporcionar una respuesta?"
+        respuesta = generar_respuesta_gpt3(prompt)
+        return respuesta if respuesta else "Lo siento, no pude encontrar la información relacionada."
     else:
-        respuesta = generar_respuesta_gpt3(consulta_usuario)
-        return respuesta if respuesta else "Lo siento, no pude encontrar una coincidencia cercana."
+        return "Lo siento, no pude encontrar la información relacionada."
+
 
 # Main function that orchestrates the above functions
 def main(consulta_usuario):
